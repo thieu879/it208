9b1563f8426e8042c3d2b01e8b00d133face474f
thieunguyen879 <nguyenthieu11022005@gmail.com>
2025-12-15 08:39:06 +0700

    ss17

---
+++ ss17/custom_addons/ss17/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "ss17",
+    "version": "1.0",
+    "category": "unknown",
+    "summary": "Custom Addons for ss17",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "authors": ["Thieu"],
+    "license": "LGPL-3",
+}
